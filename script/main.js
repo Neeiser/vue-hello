db8e1b72535585a762e48dbb4111cea6b3f9e1fc
Neeiser <stortini.study@gmail.com>
2022-03-10 16:07:54 +0100

    BONUS: IMG + Class to center

--- script/main.js
+++ script/main.js
@@ -1,6 +1,10 @@
 const root = new Vue({
     el: '#root',
     data:{
+        displaying:'center',
         message: 'Hello World',
-    }
+        btnLabel: 'Genera Immagine',
+        img: 'https://picsum.photos/500/300?random=1'
+    },
+
 })
